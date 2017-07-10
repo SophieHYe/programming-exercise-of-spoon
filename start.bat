323eeb9e5ea853c22cf86ce3982ba4cc3044bf68
SophieHYe <yehe@rfchina.com>
2017-07-11 00:21:43 +0800

    update the reminder txt in start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,7 @@
 @echo off
-echo "Please fill the path to check:"
+echo "Please fill the path of java source files to check:"
+echo Example on Windows: src\\main\\java
+
 set/p x=
 
 java -jar ./target/spoonChecker-0.0.1-SNAPSHOT-jar-with-dependencies.jar %x%
